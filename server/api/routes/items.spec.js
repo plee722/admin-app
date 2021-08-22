3d99cc2d2c7d0e30a1b94c7e8ff199c7a65af21f
Peter Lee <plee@attentivemobile.com>
2021-08-22 12:40:53 -0400

    added test files for user and item routes

---
+++ server/api/routes/items.spec.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai')
+const request = require('supertest')
+const sqlite3 = require('sqlite3')
+const app = require('../../index')
+
+const db = new sqlite3.Database('./inhome.db', (err) => {
+    if (err) {
+      throw new Error(err)
+    }
+})
+
+describe('Item routes', () => {
+    describe('/api/items', () => {
+        beforeEach(() => {
+            return db.run("INSERT INTO items (name) VALUES ('Candy')"
+        })
+
+        it('GET /api/items', async () => {
+            const res = await request(app)
+                .get('/api/items')
+                .expect(200)
+            expect(res.body).to.be.an('array')
+            expect(res.body[0].name).to.be.equal('Candy')
+        })
+    })
+})
