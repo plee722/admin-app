b1b5dcf50b217d20a68bd2a9e91a33833200443b
Peter Lee <plee@attentivemobile.com>
2021-08-24 21:46:58 -0400

    added OrderRow component for orders page

---
+++ src/components/OrderRow.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Td } from '../componentStyles';
+
+const OrderRow = ({order}) => {
+
+  return (
+    <tr>
+      <Td>{order.user_name}</Td>
+      <Td>{order.user_id}</Td>
+      <Td>{order.order_id}</Td>
+      <Td>{order.items_ordered}</Td>
+    </tr>
+  );
+};
+
+export default OrderRow;
