5565a02757b375e845c82f39b21cc0eee98335a3
Peter Lee <plee@attentivemobile.com>
2021-08-24 22:08:20 -0400

    added spec files for routes, still in progress

---
+++ test/[WIP]users.spec.js
@@ -0,0 +1,35 @@
+// const { expect } = require('chai')
+// const request = require('supertest')
+// const sqlite3 = require('sqlite3')
+// const app = require('../server/index')
+
+// const db = new sqlite3.Database('./inhome.db', (err) => {
+//     if (err) {
+//       throw new Error(err)
+//     }
+// })
+
+// describe('User routes', () => {
+//     describe('/api/users', () => {
+//         beforeEach(() => {
+//             return db.run("INSERT INTO users (name) VALUES ('Bob')")
+//         })
+
+//         it('responds with json', async (done) => {
+//             await request(app)
+//                 .get('/users')
+//                 .set('Accept', 'application/json')
+//                 .expect('Content-Type', /json/)
+//                 .expect(200, done)
+
+//         })
+
+//         it('GET /api/users', async () => {
+//             const res = await request(app)
+//                 .get('/users')
+//                 .expect(200)
+//             expect(res.body).to.be.an('object')
+//             expect(res.body.name).to.be.equal("Bob")
+//         })
+//     })
+// })
